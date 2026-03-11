aabf46ed81389d4d6b6a7cd0571e0c62d706aba1
mlau16 <mlau16@ucsc.edu>
2026-03-11 16:05:19 -0700

    Added another building from blender, added textured road, adjusted lighting

--- asgn5/asgn5.js
+++ asgn5/asgn5.js
@@ -3,10 +3,13 @@ import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
 
 const scene = new THREE.Scene();
 
-scene.fog = new THREE.FogExp2(0x38244F, 0.06);
+scene.fog = new THREE.FogExp2(0x38244F, 0.05);
 
 const cubeTextureLoader = new THREE.CubeTextureLoader();
 const skybox = cubeTextureLoader.load([
@@ -31,6 +34,19 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.toneMappingExposure = 0.45;
 document.body.appendChild( renderer.domElement );
+
+const composer = new EffectComposer(renderer);
+const renderPass = new RenderPass(scene, camera);
+composer.addPass(renderPass);
+
+const bloomPass = new UnrealBloomPass(
+  new THREE.Vector2(window.innerWidth, window.innerHeight),
+  1.5, 
+  0.4,
+  0.85
+);
+
+composer.addPass(bloomPass);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
@@ -57,13 +73,55 @@ const neonMat = new THREE.MeshBasicMaterial({
   opacity: 0.55
 });
 
+const lightning = new THREE.DirectionalLight(0xffffff, 0);
+lightning.position.set(0, 50, 0);
+scene.add(lightning);
 
 const loader = new THREE.TextureLoader();
 
+//rain
+const rainCount = 3000;
+const rainGeometry = new THREE.BufferGeometry();
+const rainPositions = new Float32Array(rainCount *3);
+const rainVelocities = new Float32Array(rainCount);
+
+for (let i = 0; i < rainCount; i++) {
+  const i3 = i * 3;
+
+  rainPositions[i3] = (Math.random() - 0.5) * 120;
+  rainPositions[i3 + 1] = Math.random() * 60 + 10;
+  rainPositions[i3 + 2] = (Math.random() - 0.5) * 120;
+
+  rainVelocities[i] = 0.5 + Math.random() * 0.8;
+}
+
+rainGeometry.setAttribute(
+  'position',
+  new THREE.BufferAttribute(rainPositions, 3)
+);
+
+const rainMaterial = new THREE.PointsMaterial({
+  color: 0xaaccee,
+  size: 0.08,
+  transparent: true,
+  opacity: 0.7
+});
+
+const rain = new THREE.Points(rainGeometry, rainMaterial);
+scene.add(rain);
+
 // road
-makeBox(6, 0.2, 36, 0x222222, 0, -1, 0);
+makeBox(50, 0.2, 50, 0x222222, 0, -1, 0);
 makeBox(3, 0.3, 36, 0x444444, -4.5, -0.9, 0);
 makeBox(3, 0.3, 36, 0x444444, 4.5, -0.9, 0);
+
+loadRoad(0, -0.85, -5, 0.8);
+loadRoad(0, -0.85, -10, 0.8);
+loadRoad(0, -0.85, -15, 0.8);
+loadRoad(0, -0.85, 0, 0.8);
+loadRoad(0, -0.85, 5, 0.8);
+loadRoad(0, -0.85, 10, 0.8);
+loadRoad(0, -0.85, 15, 0.8);
 
 // buildings
 makeBox(3, 8, 3, 0x111122, -4.5, 3.25, -4.5);
@@ -80,21 +138,23 @@ objLoader.load('/public/resources/Building1.obj', (obj) => {
     }
   });
 
-  obj.scale.set(0.7, 0.7, 0.7); // adjust size if needed
-  obj.position.set(-4.8, -1, 0); // place it in your city
+  obj.scale.set(0.7, 0.7, 0.7); 
+  obj.position.set(-4.8, -1, 0); 
 
   scene.add(obj);
 })
 
+loadBuilding(5.5, -1, 1, 1, 3.13);
+
 // rooftop details
-makeCylinder(0.8, 0.8, 1.5, 0x666666, -5, 7.8, -5);
-makeBox(1, 0.8, 1, 0x555555, 5, 9.5, -3);
-makeCylinder(0.1, 0.1, 2, 0x999999, -5, 6.5, 4);
+makeCylinder(0.8, 0.8, 1.5, 0x111122, -4.5, 7.8, -4.5);
+makeBox(1, 0.8, 1,0x111122, 5, 9.5, -3);
+makeCylinder(0.1, 0.1, 2, 0x111122, -5, 6.5, 4);
 
 // signs
-makeNeonSign(1.8, 0.7, 0.2, 0xff00ff, -3.2, 2, -5);
+makeNeonSign(1.8, 0.7, 0.2, 0xff00ff, -2.5, 2, -5);
 makeNeonSign(1.8, 0.7, 0.2, 0x00ffff, 3.2, 3, 5);
-makeNeonSign(1.5, 0.5, 0.2, 0xff44aa, 3.2, 1.5, -3);
+makeNeonSign(1.8, 0.5, 0.2, 0xff44aa, 3.2, 1.5, -3);
 makeNeonSign(0.03, 1.2, 0.5, 0xff44aa, -3.8, 2, 2.15);
 
 addWindows(-3.48, 2, 4, 1.8, 0.4);
@@ -108,11 +168,44 @@ loadLamp(3.2, -1, 3, 0.5, -7.8);
 
 camera.position.z = 5;
 
+let lightningTimer = 0;
+let nextLightning = Math.random() * 300 + 200;
+
 function animate( time ) {
 
   controls.update();
 
-  renderer.render( scene, camera );
+  const positions = rain.geometry.attributes.position.array;
+
+  for (let i = 0; i < rainCount; i++) {
+    const i3 = i * 3;
+    positions[i3 + 1] -= rainVelocities[i];
+
+    positions[i3] += 0.01;
+
+    if(positions[i3 + 1] < -1) {
+      positions[i3] = (Math.random() - 0.5) * 120;
+      positions[i3 +1] = Math.random() * 60 + 20;
+      positions[i3 + 2] = (Math.random() - 0.5) * 120;
+    }
+  }
+
+  rain.geometry.attributes.position.needsUpdate = true;
+
+  lightningTimer++;
+
+  if(lightningTimer > nextLightning) {
+    lightning.intensity = 10;
+
+    setTimeout(() => {
+      lightning.intensity = 0;
+    }, 100);
+
+    lightningTimer = 0;
+    nextLightning = Math.random() * 300 + 200;
+  }
+
+  composer.render();
 }
 renderer.setAnimationLoop( animate );
 
@@ -155,7 +248,7 @@ function makeNeonSign(w, h, d, color, x, y, z) {
   mesh.position.set(x, y, z);
   scene.add(mesh);
 
-  const glow = new THREE.PointLight(color, 2, 8);
+  const glow = new THREE.PointLight(color, 8, 18);
   glow.position.set(x + 0.3, y, z);
   scene.add(glow);
 
@@ -239,10 +332,36 @@ function loadLamp(x, y, z, scale = 1, rotationY = 0) {
 
     scene.add(lamp);
 
-    const glow = new THREE.PointLight(0xffcc88, 3, 12);
+    const glow = new THREE.PointLight(0xffcc88, 8, 18);
     glow.position.set(x, y + 2.5 * scale, z);
     scene.add(glow);
 
+  });
+
+}
+
+function loadRoad(x, y, z, scale = 1) {
+  gltfLoader.load('/public/resources/Road.glb', (gltf) => {
+    const road = gltf.scene;
+    
+    road.position.set(x, y, z);
+    road.scale.set(scale, scale, scale);
+
+    scene.add(road);
+  });
+
+}
+
+function loadBuilding(x, y, z, scale = 1, rotationY = 0) {
+  gltfLoader.load('/public/resources/building2.glb', (gltf) => {
+    const build2 = gltf.scene;
+    
+    build2.position.set(x, y, z);
+    build2.scale.set(scale, scale, scale);
+
+    build2.rotation.y = rotationY;
+
+    scene.add(build2);
   });
 
 }
